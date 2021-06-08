baf197e80f83bc814c9f260f182d054b94566629
LightningWB <63766407+LightningWB@users.noreply.github.com>
2021-06-08 13:59:50 -0500

    changed build to be better with links

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 // use this to see the documentation in a browser easily
 import * as lightspeed from "lightspeedjs";
-lightspeed({pagesLocation:'./pages/', staticPage:false});
+lightspeed({pagesLocation:'./docs/', staticPage:false});
