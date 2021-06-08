70469c13858431bea24a563e956027cd3f155faf
LightningWB <63766407+LightningWB@users.noreply.github.com>
2021-06-08 13:58:43 -0500

    few fixes to building to /docs instead of /pages

--- build/index.ts
+++ build/index.ts
@@ -13,17 +13,17 @@ let base:string = '';
 
 async function processFile(path)
 {
-	let lines:string[] = (await fs.readFile(join(__dirname, '../docs', path))).toString().split('\n');
+	let lines:string[] = (await fs.readFile(join(__dirname, '../documentation', path))).toString().split('\n');
 	let data:string = lines.join('\n');
 	data = mark(data);
 	const result = base.replace('__page__', data).replace('__title__', path.replace('.md', '')==='index' ? 'lightspeed.js' : path.replace('.md', ''));
-	await fs.writeFile(join(__dirname, '../pages/' + path.replace('.md', '.html')), result);
+	await fs.writeFile(join(__dirname, '../docs/' + path.replace('.md', '.html')), result);
 }
 
 async function main(path='')
 {
 	base = (await fs.readFile(join(__dirname, './base.html'))).toString();
-	const files = (await fs.readdir(join(__dirname, '../docs', path)));
+	const files = (await fs.readdir(join(__dirname, '../documentation', path)));
 	for(const file of files)
 	{
 		if(file.indexOf('.')===-1)main(file);
